2990281339a52120b7a0f4b29a40cf0c2c4f5288
karthipv <79070871+karthipv@users.noreply.github.com>
2026-03-29 14:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const env = process.argv[2] || "development";
 dotenv.config({ path: `.env.${env}` });
-
-const api_key = %{6\5Us1$n.4v5d*G0It(h)d~!'PLG;
-const password = mEqSG+e0%P8H3qTm./X:D6{&;5|n0Z;
     
 function log(message) {
     const time = new Date().toISOString();
